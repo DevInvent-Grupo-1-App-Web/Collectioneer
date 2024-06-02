eb03eaf157f4c0dd9edfefd39a82f798a24e3559
ChamorroSamuel <102174595+ChamorroSamuel@users.noreply.github.com>
2024-06-02 00:02:09 -0500

    State in Auction

--- lib/ui/screens/auction/convert_an_aucttion_screen.dart
+++ lib/ui/screens/auction/convert_an_aucttion_screen.dart
@@ -4,7 +4,8 @@ class ConvertAnAuctionScreen extends StatefulWidget {
   const ConvertAnAuctionScreen({Key? key}) : super(key: key);
 
   @override
-  _ConvertAnAuctionScreenState createState() => _ConvertAnAuctionScreenState();
+  _ConvertAnAuctionScreenState createState() =>
+      _ConvertAnAuctionScreenState();
 }
 
 class _ConvertAnAuctionScreenState extends State<ConvertAnAuctionScreen> {
@@ -38,46 +39,109 @@ class _ConvertAnAuctionScreenState extends State<ConvertAnAuctionScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.vertical(top: Radius.zero),
-                  child: Image.asset(
-                    'assets/images/sample_image.jpg',
-                    height: 150,
-                    fit: BoxFit.cover,
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.vertical(top: Radius.zero),
+                    child: Image.asset(
+                      'assets/images/sample_image.jpg',
+                      height: 150,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                "Título",
-                style: TextStyle(fontSize: 18, fontFamily: 'fontFamily'),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                title,
-                style: const TextStyle(fontSize: 16, fontFamily: 'fontFamily'),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                "Descripción",
-                style: TextStyle(fontSize: 18, fontFamily: 'fontFamily'),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                description,
-                style: const TextStyle(fontSize: 16, fontFamily: 'fontFamily'),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                "Estado",
-                style: TextStyle(fontSize: 18, fontFamily: 'fontFamily'),
-              ),
-              const SizedBox(height: 8),
-            ],
+                const SizedBox(height: 20),
+                const Text(
+                  "Título",
+                  style: TextStyle(fontSize: 18, fontFamily: 'fontFamily'),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  title,
+                  style: const TextStyle(fontSize: 16, fontFamily: 'fontFamily'),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Descripción",
+                  style: TextStyle(fontSize: 18, fontFamily: 'fontFamily'),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  description,
+                  style: const TextStyle(fontSize: 16, fontFamily: 'fontFamily'),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Estado",
+                  style: TextStyle(fontSize: 18, fontFamily: 'fontFamily'),
+                ),
+                const SizedBox(height: 8),
+                DropdownButtonFormField<String>(
+                  value: selectedOption,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedOption = newValue!;
+                    });
+                  },
+                  items: items.map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(height: 20),
+                // Muestra los campos de precio y tiempo si se selecciona Subasta
+                if (selectedOption == "Subasta")
+                  Column(
+                    children: [
+                      const Text(
+                        "Precio Base:",
+                        style:
+                            TextStyle(fontSize: 18, fontFamily: 'fontFamily'),
+                      ),
+                      const SizedBox(height: 8),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Ingresa el precio",
+                          border: OutlineInputBorder(),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            price = value;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Tiempo (Hrs):",
+                        style:
+                            TextStyle(fontSize: 18, fontFamily: 'fontFamily'),
+                      ),
+                      const SizedBox(height: 8),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Ingresa el tiempo",
+                          border: OutlineInputBorder(),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            time = value;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+              ],
+            ),
           ),
         ),
       ),
