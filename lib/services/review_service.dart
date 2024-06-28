d907c3cbee6776b2b31f6baad3f002a1492c7444
Diego Fabian Cancho Coila <u20201f479@upc.edu.pe>
2024-06-28 11:15:30 -0500

    feat: review support

---
+++ lib/services/review_service.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:collectioneer/models/review.dart';
+import 'package:collectioneer/services/base_service.dart';
+import 'package:collectioneer/services/models/review_request.dart';
+import 'package:collectioneer/user_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class ReviewService extends BaseService {
+  Future<Review> postReview(ReviewRequest reviewRequest) async {
+    final req = (Uri.parse('$baseUrl/collectible/new-review'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ${UserPreferences().getUserToken()}',
+      },
+      body: jsonEncode(reviewRequest.toJson()),
+    );
+
+    log(req.toString());
+
+    final response = await http.post(
+      Uri.parse('$baseUrl/collectible/new-review'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ${UserPreferences().getUserToken()}',
+      },
+      body: jsonEncode(reviewRequest.toJson()),
+    );
+
+    log(response.body);
+
+    if (response.statusCode != 200) {
+      throw Exception(response.body);
+    }
+
+    final Map<String, dynamic> body = jsonDecode(response.body);
+    return Review.fromJson(body);
+  }
+
+  Future<List<Review>> getReviewsForCollectible(int collectibleId) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/collectible/$collectibleId/reviews'),
+      headers: <String, String>{
+        'Authorization': 'Bearer ${UserPreferences().getUserToken()}',
+      },
+    );
+    
+    if (response.statusCode != 200) {
+      throw Exception(response.body);
+    }
+
+    final List<dynamic> body = jsonDecode(response.body);
+    log(body.toString());
+
+    return body.map((dynamic item) => Review.fromJson(item)).toList();
+
+  }
+}
--- lib/ui/screens/common/interactions_bottom_sheet.dart
+++ lib/ui/screens/common/interactions_bottom_sheet.dart
@@ -1,7 +1,10 @@
 import 'package:collectioneer/models/comment.dart';
 import 'package:collectioneer/models/element_type.dart';
+import 'package:collectioneer/models/review.dart';
 import 'package:collectioneer/services/comment_service.dart';
 import 'package:collectioneer/services/models/comment_request.dart';
+import 'package:collectioneer/services/models/review_request.dart';
+import 'package:collectioneer/services/review_service.dart';
 import 'package:collectioneer/user_preferences.dart';
 import 'package:flutter/material.dart';
 
@@ -17,10 +20,8 @@ class _InteractionBottomSheetState extends State<InteractionBottomSheet> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: CommentScreen(type: widget.type),
-    );
+    return widget.type == ElementType.collectible ? 
+    DoubleInteraction(type: widget.type) : CommentScreen(type: widget.type);
   }
 }
 
@@ -35,13 +36,16 @@ class CommentScreen extends StatefulWidget {
 class _CommentScreenState extends State<CommentScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          child: CommentsList(type: widget.type,),
-        ),
-        CommentInput(type: widget.type,),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: CommentsList(type: widget.type,),
+          ),
+          CommentInput(type: widget.type,),
+        ],
+      ),
     );
   }
 }
@@ -94,6 +98,54 @@ class _CommentInputState extends State<CommentInput> {
   }
 }
 
+class DoubleInteraction extends StatefulWidget {
+  const DoubleInteraction({super.key, required this.type});
+  final ElementType type;
+
+  @override
+  State<DoubleInteraction> createState() => _DoubleInteractionState();
+}
+
+class _DoubleInteractionState extends State<DoubleInteraction> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(initialIndex: 0, vsync: this, length: 2);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: 'Comentarios'),
+            Tab(text: 'Reseñas'),
+          ],
+        ),
+        Expanded(
+          child: TabBarView(
+            controller: _tabController,
+            children: [
+              CommentScreen(type: widget.type),
+              ReviewScreen()
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class CommentsList extends StatefulWidget {
   const CommentsList({super.key, required this.type});
   final ElementType type;
@@ -122,21 +174,15 @@ class _CommentsListState extends State<CommentsList> {
             itemBuilder: (context, index) {
               final comment = comments[index];
               return Card(
-                elevation: 1,
                 margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                 child: Padding(
                   padding: const EdgeInsets.all(12.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Row(
-                        children: [
-                          const SizedBox(width: 12),
-                          Text(
-                            comment.authorName,
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ],
+                      Text(
+                        comment.authorName,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
                       ),
                       const SizedBox(height: 8),
                       Text(comment.body),
@@ -152,3 +198,150 @@ class _CommentsListState extends State<CommentsList> {
   }
 }
 
+class ReviewsList extends StatefulWidget {
+  const ReviewsList({super.key});
+
+  @override
+  State<ReviewsList> createState() => _ReviewsListState();
+}
+
+class _ReviewsListState extends State<ReviewsList> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: ReviewService().getReviewsForCollectible(UserPreferences().getActiveElement()),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          final reviews = snapshot.data as List<Review>;
+          return ListView.builder(
+            itemCount: reviews.length,
+            itemBuilder: (context, index) {
+              final review = reviews[index];
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        review.userId.toString(),
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(review.content),
+                      const SizedBox(height: 8),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: List.generate(5, (index) {
+                          if (index < review.rating) {
+                            return Icon(Icons.star, size: 16, color: Theme.of(context).colorScheme.primary);
+                          } else {
+                            return Icon(Icons.star_outline, size: 16, color: Theme.of(context).colorScheme.primary);
+                          }
+                        }),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        }
+      
+      },
+    );
+  }
+}
+
+class ReviewScreen extends StatefulWidget {
+  const ReviewScreen({super.key});
+
+  @override
+  State<ReviewScreen> createState() => _ReviewScreenState();
+}
+
+class _ReviewScreenState extends State<ReviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: ReviewsList(),
+          ),
+          ReviewInput(),
+        ],
+      ),
+    );
+  }
+}
+
+class ReviewInput extends StatefulWidget {
+  const ReviewInput({super.key});
+
+  @override
+  State<ReviewInput> createState() => _ReviewInputState();
+}
+
+class _ReviewInputState extends State<ReviewInput> {
+  final TextEditingController _controller = TextEditingController();
+  int _rating = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(children:
+          List.generate(
+            5,
+            (index) => IconButton(
+              icon: Icon(index < _rating ? Icons.star : Icons.star_outline),
+              color: index < 5 ? Theme.of(context).colorScheme.primary : null,
+              onPressed: () {
+                setState(() {
+                  _rating = index + 1;
+                });
+              },
+            ),
+          )
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Escribe una reseña...',
+                ),
+                controller: _controller,
+              ),
+            ),
+            const SizedBox(width: 8,),
+            IconButton(
+              icon: const Icon(Icons.send),
+              onPressed: () async {
+                if (_controller.text.isNotEmpty) {
+                  final review = ReviewRequest(
+                    reviewerId: UserPreferences().getUserId(),
+                    collectibleId: UserPreferences().getActiveElement(),
+                    content: _controller.text,
+                    rating: _rating
+                  );
+                  
+                  await ReviewService().postReview(review);
+        
+                  _controller.clear();
+                }
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
