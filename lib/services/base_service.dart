352e05523d8158a1748a7efbf787910ba2220d2d
dcancho <diegocancho@outlook.com>
2024-08-29 21:57:38 -0500

    code: update endpoint url

--- lib/services/base_service.dart
+++ lib/services/base_service.dart
@@ -1,7 +1,7 @@
 import 'package:collectioneer/user_preferences.dart';
 
 class BaseService {
-  final String baseUrl = 'https://api.mycollectioneer.net';
+  final String baseUrl = 'https://collectioneer.azurewebsites.net';
 
   // Method to add the token to the headers
   Map<String, String> getHeaders() {
